0941256498283dfb9cfb9fc9c5dc57523bab4571
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-23 16:24:34 +0500

    'Compiles'

--- src/app/component/TodoList.js
+++ src/app/component/TodoList.js
@@ -6,8 +6,8 @@ const TodoList = () => {
             <>
                 <div className="nav flex flex-row justify-between items-center py-4 max-w-3xl border mx-auto container px-4 my-4 gap-3">
                     <div>
-                        <p className='font-bold text-2xl'>Tailwind Css</p>
-                        <p className='font-bold flex-col'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+                        <p className='font-bold text-1xl'>Tailwind Css</p>
+                        <p className='flex-col'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
                     </div>
                     <div className='flex flex-row justify-start items-start gap-6'>
                     <RiEdit2Line className='cursor-pointer' size={24} /> 
