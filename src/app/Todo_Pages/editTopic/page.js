0fa4c40ce99ba84d45832b41bc8c93709f8eb689
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-23 19:36:46 +0500

    'Next_App_App'

--- src/app/Todo_Pages/editTopic/page.js
+++ src/app/Todo_Pages/editTopic/page.js
@@ -4,8 +4,10 @@ const page = () => {
   return (
     <div>
         <NavbarTodo />
-       <div>
-        <input />
+       <div className='mx-auto max-w-3xl flex flex-col gap-6'>
+        <input className='border border-slate-500 px-6 py-2' placeholder='Update Title' />
+        <input className='border border-slate-500 px-6 py-2' placeholder='Update Description' />
+        <button className='bg-green-500 px-2 py-2 font-bold text-white w-32'>Update Topic</button>
        </div>
     </div>
   )
