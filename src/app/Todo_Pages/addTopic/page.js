94986b38abcf10bc3065106975bfca3f967889ab
junaidsalam639 <junaidsalam639@gmail.com>
2023-10-23 19:39:40 +0500

    'Next_App_Create'

--- src/app/Todo_Pages/addTopic/page.js
+++ src/app/Todo_Pages/addTopic/page.js
@@ -5,10 +5,10 @@ const page = () => {
     return (
         <div>
             <NavbarTodo />
-            <div className='mx-auto gap-3 flex flex-col max-w-3xl'>
+            <div className='mx-auto gap-6 flex flex-col max-w-3xl'>
                 <input placeholder='Topic Title' className='border border-slate-500 px-8 py-2' />
                 <input placeholder='Topic Description' className='border border-slate-500 px-8 py-2' />
-                <button className='bg-green-500 py-3 w-32 px-3'>Add Topic</button>
+                <button className='bg-green-500 py-3 w-32 px-3 font-bold text-white'>Add Topic</button>
             </div>
         </div>
     )
